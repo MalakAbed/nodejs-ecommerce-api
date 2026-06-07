34c29046e58a4a5f7332ee7d307d401a412343a7
MalakAbed <malakabed.1999@gmail.com>
2026-06-07 10:08:07 +0300

    update

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url:'mongodb+srv://malakabed:Df43MbyEI4ku9azT@cluster0.yfpykjt.mongodb.net/ecommerce-database?retryWrites=true&w=majority&appName=Cluster0'
+  url:MONGOBD_URI
 };
